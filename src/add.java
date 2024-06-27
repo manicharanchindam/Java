9b744f0f5ad7c0330621e6218a4bcf3473e218aa
manicharanchindam <manicharanchindam@email.com>
2024-06-27 12:52:23 +0530

    array

---
+++ src/add.java
@@ -0,0 +1,9 @@
+public class add {
+    public static void main(String[] args) {
+        int a=10;
+        double b=24.612647184715645;
+        double c= a+b;
+        System.out.println(c);
+    }
+    
+}
---
+++ src/array.java
@@ -0,0 +1,12 @@
+public class array {
+    public static void main(String[] args){
+        int[] id = {1,2,3,4,5};
+        int len=id.length;
+        //System.out.println(id[2]);
+for (int i = 0; i < len; i++) {
+    System.out.println(id[i]);
+    
+}
+    }
+    
+}
